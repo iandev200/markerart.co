91a961465ac23a62b87468e33db3a2599b1e7086
Ian Escoto <iescoto.web@gmail.com>
2022-10-11 22:42:42 -0700

    form

--- src/pages/Questionnaire.vue
+++ src/pages/Questionnaire.vue
@@ -16,39 +16,24 @@
       <section class="relative">
         <div class="max-w-6xl mx-auto px-4 sm:px-6 relative">
           <div class="pt-32 pb-12 md:pt-40 md:pb-20">
-            <!-- Page header -->
-            <!-- <div class="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-              <h1 class="h1 mb-4" data-aos="fade-up">How can we help you?</h1>
-              <p
-                class="text-xl text-gray-400"
-                data-aos="fade-up"
-                data-aos-delay="200"
-              >
-                We have custom plans to power your business. Tell us your needs,
-                and we’ll contact you shortly.
-              </p>
-            </div> -->
-
             <!-- Contact form -->
-            <form class="max-w-xl mx-auto">
+            <form ref="form" class="max-w-xl mx-auto">
               <div class="flex flex-wrap -mx-3 mb-4">
                 <div class="w-full px-3">
                   <label
                     class="block text-gray-300 text-sm font-medium mb-1"
                     for="first-name"
-                    >First Name <span class="text-red-600">*</span></label
+                    >Name</label
                   >
                   <input
                     id="first-name"
                     v-model="name"
+                    label="name"
                     type="text"
-                    class="form-input w-full text-gray-300 border-red-500 focus:border-red-500"
+                    class="form-input w-full text-gray-300"
                     placeholder="Enter your first name"
-                    required
+                    :rules="nameRules"
                   />
-                  <p class="text-red-500 text-sm mt-2">
-                    This field is required
-                  </p>
                 </div>
               </div>
               <div class="flex flex-wrap -mx-3 mb-4">
@@ -56,15 +41,16 @@
                   <label
                     class="block text-gray-300 text-sm font-medium mb-1"
                     for="email"
-                    >Email <span class="text-red-600">*</span></label
+                    >Email</label
                   >
                   <input
                     v-model="email"
                     id="email"
+                    label="email"
                     type="email"
                     class="form-input w-full text-gray-300"
                     placeholder="Enter your email address"
-                    required
+                    :rules="emailRules"
                   />
                 </div>
               </div>
@@ -81,6 +67,7 @@
                     rows="4"
                     class="form-textarea w-full text-gray-300"
                     placeholder="Write your message"
+                    :rules="req"
                   ></textarea>
                 </div>
               </div>
@@ -121,11 +108,11 @@ export default {
   },
   methods: {
     async submit() {
-      //add validation here
-      if (this.name == "" || this.email == "" || this.message == "") {
-        alert("Please fill out");
-        return;
-      }
+      //!!!!!!!add validation here
+      // if (this.name == "" || this.email == "" || this.message == "") {
+      //   alert("Please fill out");
+      //   return;
+      // }
       const docRef = await addDoc(collection(db, "leads"), {
         name: this.name,
         email: this.email,
@@ -136,9 +123,16 @@ export default {
   },
   data() {
     return {
+      valid: true,
       name: "",
+      nameRules: [(v) => !!v || "Name is required"],
       email: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
       message: "",
+      req: [(v) => !!v || "Field is required"],
     };
   },
 };
