135bb73805b8a56472fa4d03c894f55b86723cb8
Ian Escoto <iescoto.web@gmail.com>
2023-07-26 20:46:24 -0700

    PreValidation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
+import { Field, Form, ErrorMessage, defineRule, configure } from "vee-validate";
+import { required, email } from "@vee-validate/rules";
 
-import "./css/style.scss";
+// Use the Vee-Validate components
 const app = createApp(App);
 app.use(router);
+app.component("Form", Form);
+app.component("Field", Field);
+app.component("ErrorMessage", ErrorMessage);
+
+// Define the validation rules and messages
+defineRule("required", required);
+defineRule("email", email);
+
+// Configure the validation messages
+configure({
+  generateMessage: (context) => {
+    const messages = {
+      required: "This field is required.",
+      email: "Please enter a valid email address.",
+    };
+    const message = messages[context.rule.name];
+    return message ? message : `The ${context.field} field is invalid.`;
+  },
+});
+
 app.mount("#app");
--- src/pages/Questionnaire.vue
+++ src/pages/Questionnaire.vue
@@ -17,23 +17,23 @@
         <div class="max-w-6xl mx-auto px-4 sm:px-6 relative">
           <div class="pt-32 pb-12 md:pt-40 md:pb-20">
             <!-- Contact form -->
-            <form ref="form" class="max-w-xl mx-auto">
+            <form @submit="submit" class="max-w-xl mx-auto">
               <div class="flex flex-wrap -mx-3 mb-4">
                 <div class="w-full px-3">
                   <label
                     class="block text-gray-300 text-sm font-medium mb-1"
-                    for="first-name"
+                    for="name"
                     >Name</label
                   >
-                  <input
-                    id="first-name"
-                    v-model="name"
-                    label="name"
+                  <Field
+                    name="name"
                     type="text"
                     class="form-input w-full text-gray-300"
                     placeholder="Enter your first name"
+                    v-model="values.name"
                     :rules="nameRules"
                   />
+                  <span class="text-red-500">{{ errors.first("name") }}</span>
                 </div>
               </div>
               <div class="flex flex-wrap -mx-3 mb-4">
@@ -43,15 +43,15 @@
                     for="email"
                     >Email</label
                   >
-                  <input
-                    v-model="email"
-                    id="email"
-                    label="email"
+                  <Field
+                    name="email"
                     type="email"
                     class="form-input w-full text-gray-300"
                     placeholder="Enter your email address"
+                    v-model="values.email"
                     :rules="emailRules"
                   />
+                  <span class="text-red-500">{{ errors.first("email") }}</span>
                 </div>
               </div>
               <div class="flex flex-wrap -mx-3 mb-4">
@@ -61,22 +61,25 @@
                     for="message"
                     >Message</label
                   >
-                  <textarea
-                    v-model="message"
-                    id="message"
+                  <Field
+                    name="message"
+                    type="textarea"
                     rows="4"
                     class="form-textarea w-full text-gray-300"
                     placeholder="Write your message"
+                    v-model="values.message"
                     :rules="req"
-                  ></textarea>
+                  />
+                  <span class="text-red-500">{{
+                    errors.first("message")
+                  }}</span>
                 </div>
               </div>
               <div class="flex flex-wrap -mx-3 mt-6">
                 <div class="w-full px-3">
                   <button
                     class="btn text-white bg-purple-600 hover:bg-purple-700 w-full"
-                    @click="submit"
-                    type="button"
+                    type="submit"
                   >
                     Send
                   </button>
@@ -95,8 +98,9 @@ import Header from "../partials/Header.vue";
 import PageIllustration from "../partials/PageIllustration.vue";
 import CtaContact from "../partials/CtaContact.vue";
 import Footer from "../partials/Footer.vue";
-import db from "../firebase";
+import { db } from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
+import { useField, useForm } from "vee-validate";
 
 export default {
   name: "Questionnaire",
@@ -106,31 +110,28 @@ export default {
     CtaContact,
     Footer,
   },
-  methods: {
-    async submit() {
-      if (!this.$refs.form.validate()) {
-        return;
+  setup() {
+    const { errors, values, validate, resetForm, handleSubmit } = useForm();
+
+    async function submit() {
+      try {
+        await validate();
+        const docRef = await addDoc(collection(db, "leads"), {
+          name: values.name,
+          email: values.email,
+          message: values.message,
+        });
+        console.log("Document written with ID:", docRef.id);
+        resetForm();
+      } catch (error) {
+        console.error("Error adding document:", error);
       }
-      const docRef = await addDoc(collection(db, "leads"), {
-        name: this.name,
-        email: this.email,
-        message: this.message,
-      });
-      console.log("Document written with ID: ", docRef.id);
-    },
-  },
-  data() {
+    }
+
     return {
-      valid: true,
-      name: "",
-      nameRules: [(v) => !!v || "Name is required"],
-      email: "",
-      emailRules: [
-        (v) => !!v || "E-mail is required",
-        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
-      ],
-      message: "",
-      req: [(v) => !!v || "Field is required"],
+      values,
+      errors,
+      submit,
     };
   },
 };
